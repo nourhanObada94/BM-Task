cd50b538c2b032a789f00501547ad4da4da7aa7a
NourhanObada <nourhanobada94@gmail.com>
2024-08-01 17:12:51 +0300

    Added db components to Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,3 +23,21 @@ resource "azurerm_app_service" "demo-app-service" {
   resource_group_name = azurerm_resource_group.demo.name
   app_service_plan_id = azurerm_app_service_plan.demo.id
 }
+
+resource "azurerm_sql_server" "mydbsqlserver" {
+  name                         = "mydb-sql-server"
+  resource_group_name          = azurerm_resource_group.demo.name
+  location                     = azurerm_resource_group.demo.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "Password123"
+}
+
+resource "azurerm_sql_database" "mydb" {
+  name                = "mydb"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+  server_name         = azurerm_sql_server.mydbsqlserver.name
+  edition             = "Basic"
+}
+
